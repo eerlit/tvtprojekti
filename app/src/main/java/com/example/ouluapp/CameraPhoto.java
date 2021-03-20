6de326ea41b567cf9e3b789e9b0ebb264a0067e1
vekeI-at-787961774670 <veikka.mikkola@gmail.com>
2021-03-20 15:59:52 +0200

    Kartaan lisätty kameroiden iconit

--- app/src/main/java/com/example/ouluapp/CameraPhoto.java
+++ app/src/main/java/com/example/ouluapp/CameraPhoto.java
@@ -20,9 +20,8 @@ public class CameraPhoto extends AppCompatActivity {
 
     private ImageView imageView;
     private TextView textView;
-    String[] cameraPhoto;
-    String[] cameraName;
-    Float x1, x2, y1, y2;
+    String[] cameraPhoto, cameraName, time, cameraDirection;
+    Float x1, x2;
     int cameraPhotoLength = 0;
     int photoID = 0;
     Context context;
@@ -36,63 +35,29 @@ public class CameraPhoto extends AppCompatActivity {
         Intent intent = getIntent();
         context = getApplicationContext();
 
-
+        cameraDirection = (String[])intent.getSerializableExtra("CAMERA_DIRECTION");
         cameraPhoto = (String[])intent.getSerializableExtra("CAMERA");
         cameraName = (String[])intent.getSerializableExtra("CAMERA_NAME");
+        time = (String[])intent.getSerializableExtra("TIME");
+
         cameraPhotoLength= cameraPhoto.length-1;
         Picasso.with(context).load(cameraPhoto[photoID]).into(imageView);
-        textView.setText(cameraName[photoID]);
-
-
-        imageView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent touchevent) {
-                switch (touchevent.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        x1 = touchevent.getX();
-                        y1 = touchevent.getY();
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        x2 = touchevent.getX();
-                        y2 = touchevent.getY();
-                        if (x1 < x2) {
-                            photoID++;
-                            if(photoID > cameraPhotoLength){
-                                photoID = 0;
-                            }
-                            Picasso.with(context).load(cameraPhoto[photoID]).into(imageView);
-
-                        }
-                        break;
-
-                }
-                return false;
-            }
-
-        });
-
-
-
+        switchPhoto(photoID);
 
     }
     public boolean onTouchEvent(MotionEvent touchevent){
         switch (touchevent.getAction()){
             case MotionEvent.ACTION_DOWN:
                 x1 = touchevent.getX();
-                y1 = touchevent.getY();
                 break;
             case MotionEvent.ACTION_UP:
                 x2 = touchevent.getX();
-                y2 = touchevent.getY();
                 if(x1 < x2){
                     photoID++;
                     if(photoID > cameraPhotoLength){
                         photoID = 0;
                     }
-                    System.out.println("PHOTOLENGTH: " +cameraPhotoLength);
-                    System.out.println("PHOTOID: " +photoID);
-                    Picasso.with(context).load(cameraPhoto[photoID]).into(imageView);
-                    textView.setText(cameraName[photoID]);
+                    switchPhoto(photoID);
 
                 }
                 if(x1 > x2){
@@ -100,11 +65,25 @@ public class CameraPhoto extends AppCompatActivity {
                     if(photoID == -1){
                         photoID = cameraPhotoLength;
                     }
-                    Picasso.with(context).load(cameraPhoto[photoID]).into(imageView);
-                    textView.setText(cameraName[photoID]);
+                    switchPhoto(photoID);
                 }
                 break;
         }
         return false;
+    }
+    public void switchPhoto(int id){
+        Picasso.with(context).load(cameraPhoto[photoID]).into(imageView);
+        String[] dateParts = time[photoID].split("T");
+        String[] timeParts = dateParts[1].split("z");
+        String[] finalTimeParts = timeParts[0].split("\\.");
+
+
+        String[] dateSplit = dateParts[0].split("-");
+        String finaldate = dateSplit[2] + "." + dateSplit[1] + "." + dateSplit[0];
+        
+        String dateTime = finaldate + " " + finalTimeParts[0];
+
+        textView.setText(cameraName[id] + "\n" + cameraDirection[id] + "\n" + dateTime);
+        System.out.println(finalTimeParts[0]);
     }
 }
--- app/src/main/java/com/example/ouluapp/MainActivity.java
+++ app/src/main/java/com/example/ouluapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -11,8 +12,6 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
-import com.apollographql.apollo.ApolloClient;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
@@ -27,50 +26,38 @@ import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
 import org.jetbrains.annotations.NotNull;
 
 
-
 public class MainActivity extends AppCompatActivity{
     private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
     private MapView map = null;
     private  MyLocationNewOverlay mLocationOverlay = null;
-    public PlaceholderAPI placeholderAPI;
+
 
     List<GetAllCamerasQuery.Camera> cameras = new ArrayList<>();
     ArrayList<OverlayItem> items = new ArrayList<OverlayItem>();
     Context ctx;
-    String[] cameraURL;
-    String[] cameraName;
-
-
+    private String[] cameraURL, cameraDirection, cameraTime, cameraName;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //handle permissions first, before map is created. not depicted here
         getAllCameras();
-        //load/initialize the osmdroid configuration, this can be done
         ctx = getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
-        //setting this before the layout is inflated is a good idea
-        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
-        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
-        //see also StorageUtils
-        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's
-        //tile servers will get you banned based on this string
 
-        //inflate and create the map
         setContentView(R.layout.activity_main);
 
         map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
+
 
         IMapController mapController = map.getController();
         mapController.setZoom(14);
@@ -82,40 +69,47 @@ public class MainActivity extends AppCompatActivity{
         map.getOverlays().add(this.mLocationOverlay);
 
         requestPermissionsIfNecessary(new String[] {
-                // if you need to show the current location, uncomment the line below
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION,
-                // WRITE_EXTERNAL_STORAGE is required in order to show the map
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
         });
 
+
     }
 
+
     private  void updatemap(){
-        System.out.println("UpdateMap"+ cameras.size());
-        for (int i = 0; i < cameras.size(); i++){
-            System.out.println("forloop");
-            items.add(new OverlayItem( cameras.get(i).name, "Kamera",new GeoPoint(cameras.get(i).lat.doubleValue(), cameras.get(i).lon.doubleValue())));
-
+        for (int i = 0; i < cameras.size(); i++) {
+            OverlayItem olItem = new OverlayItem(cameras.get(i).name, "Kamera", new GeoPoint(cameras.get(i).lat.doubleValue(), cameras.get(i).lon.doubleValue()));
+            Drawable newMarker = ctx.getResources().getDrawable(R.drawable.ic_baseline_camera_alt_24);
+            olItem.setMarker(newMarker);
+            items.add(olItem);
         }
-
 
         ItemizedOverlayWithFocus<OverlayItem> mOverlay = new ItemizedOverlayWithFocus<OverlayItem>(items,
                 new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+
+
                     @Override
                     public boolean onItemSingleTapUp(final int index, final OverlayItem item) {
                         Intent intent = new Intent(ctx, CameraPhoto.class);
                         cameraURL = new String[cameras.get(index).presets.size()];
+                        cameraDirection = new String[cameras.get(index).presets.size()];
+                        cameraTime = new String[cameras.get(index).presets.size()];
                         cameraName = new String[cameras.get(index).presets.size()];
 
                         for(int i = 0; i < cameras.get(index).presets.size(); i++){
                             Log.d("kamerat", cameras.get(index).presets.get(i).imageUrl);
-                            cameraName[i] = cameras.get(index).presets.get(i).presentationName;
+                            cameraDirection[i] = cameras.get(index).presets.get(i).presentationName;
                             cameraURL[i] = cameras.get(index).presets.get(i).imageUrl;
+                            cameraTime[i] = cameras.get(index).presets.get(i).measuredTime;
+                            cameraName[i] = cameras.get(index).name;
 
                         }
+                        intent.putExtra("CAMERA_DIRECTION", cameraDirection);
                         intent.putExtra("CAMERA_NAME", cameraName);
                         intent.putExtra("CAMERA", cameraURL);
+                        intent.putExtra("TIME",cameraTime);
                         startActivity(intent);
                         return true;
                     }
@@ -124,8 +118,6 @@ public class MainActivity extends AppCompatActivity{
                         return false;
                     }
                 }, this);
-
-        mOverlay.setFocusItemsOnTap(true);
 
         map.getOverlays().add(mOverlay);
     }
@@ -138,17 +130,13 @@ public class MainActivity extends AppCompatActivity{
                 .enqueue(new ApolloCall.Callback<GetAllCamerasQuery.Data>() {
                     @Override
                     public void onResponse(@NotNull Response<GetAllCamerasQuery.Data> response) {
-                        Log.d("MainActivity", "Response: " + response.data().cameras);
-
                         cameras = response.data().cameras;
-
                         updatemap();
-
                     }
 
                     @Override
                     public void onFailure(@NotNull ApolloException e) {
-                        Log.d("MAINACTIVITY", "Exception " + e.getMessage(), e);
+                        Log.d("MainActigetAllCameras", "Exception " + e.getMessage(), e);
                     }
                 });
 
